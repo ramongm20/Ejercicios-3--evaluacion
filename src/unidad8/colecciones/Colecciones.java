f4487da9e058d5ed5ca4cbc4edd08e7dc4c30f4d
Ramón García <ramongm20@educastur.es>
2021-04-22 12:36:29 +0200

    Ejercicio 3 de la unidad 8 (colecciones 2) resuelto.

--- src/unidad8/colecciones/Colecciones.java
+++ src/unidad8/colecciones/Colecciones.java
@@ -1,5 +1,6 @@
 package unidad8.colecciones;
 
+import java.util.Map;
 import java.util.Set;
 
 public class Colecciones {
@@ -10,6 +11,10 @@ public class Colecciones {
 
 	static boolean contieneImpares(Set<Integer> enteros) {
 		return enteros.stream().anyMatch(n -> n % 2 == 1);
+	}
+
+	static boolean valoresUnicos(Map<String, String> mapa) {
+		return mapa.values().stream().distinct().count() != mapa.values().size();
 	}
 
 }
