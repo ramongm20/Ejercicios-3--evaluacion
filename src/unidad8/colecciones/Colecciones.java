8135f8761a2b4d978833b0c81cc8a535af243599
Ramón García <ramongm20@educastur.es>
2021-04-22 18:13:23 +0200

    Ejercicio 12 de la unidad 8 (colecciones 2) resuelto.

--- src/unidad8/colecciones/Colecciones.java
+++ src/unidad8/colecciones/Colecciones.java
@@ -1,6 +1,8 @@
 package unidad8.colecciones;
 
+import java.util.Deque;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -18,19 +20,31 @@ public class Colecciones {
 	static boolean valoresUnicos(Map<String, String> mapa) {
 		return mapa.values().stream().distinct().count() != mapa.values().size();
 	}
-static boolean algunaSeRepiteAlMenos3Veces(List<String> lista) {
-	Map<String, Integer> mapa = new HashMap<>();
-	for (String s: lista)
-		if (mapa.containsKey(s))
-			if (mapa.get(s) == 2)
-				return true;
-			else
-				mapa.put(s, mapa.get(s) + 1);
-		else
-			mapa.put(s, 1);
-	return false;
-	
-//	return lista.stream().collect(HashMap<String, Integer>::new, (a, b) -> a.put(b, a.containsKey(b) ? a.get(b) + 1 : 1), (a, b) -> {}).values().stream().anyMatch(value -> value >= 3);
 
-}
+	static boolean algunaSeRepiteAlMenos3Veces(List<String> lista) {
+		Map<String, Integer> mapa = new HashMap<>();
+		for (String s : lista)
+			if (mapa.containsKey(s))
+				if (mapa.get(s) == 2)
+					return true;
+				else
+					mapa.put(s, mapa.get(s) + 1);
+			else
+				mapa.put(s, 1);
+		return false;
+
+//	return lista.stream().collect(HashMap<String, Integer>::new, (a, b) -> a.put(b, a.containsKey(b) ? a.get(b) + 1 : 1), (a, b) -> {}).values().stream().anyMatch(value -> value >= 3);
+	}
+	
+	public static void duplicar(Deque<Integer> pila) {
+		Deque<Integer> aux = new LinkedList<>();
+		while (!pila.isEmpty())
+			aux.push(pila.pop());
+		while (!aux.isEmpty()) {
+			int n = aux.pop();
+			pila.push(n);
+			pila.push(n);
+		}
+	}
+
 }
